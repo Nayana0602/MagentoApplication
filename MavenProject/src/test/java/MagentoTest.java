7c324e948871cbde713947d0186d1fee37c02dab
user <user@LAPTOP-DGI66V1F>
2019-01-22 15:03:15 +0530

    first push

---
+++ MavenProject/src/test/java/MagentoTest.java
@@ -0,0 +1,44 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class MagentoTest 
+{
+	public void magento() 
+	{
+		String url="http://www.magento.com";
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+	
+		
+		
+		Home h= new Home(driver);
+		h.clickOnMyAcc();
+		
+		Login l =new Login(driver);
+		l.clickOnLogin();
+		l.typeEmail("nitinmanjunath1991@gmail.com");
+		l.typePassword("Welcome123");
+		l.clickOnLogin();
+		
+		
+		Logout l1 = new Logout(driver);
+		l1.clickOnLogout();
+		
+		driver.quit();
+		
+		
+		
+		
+		
+		
+	}
+}
+
